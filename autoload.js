dec8850a9e9a8028d78f82b8c9dfbbfee8471afb
fupi <fupicat@gmail.com>
2020-03-03 16:09:53 -0300

    disqus test

--- autoload.js
+++ autoload.js
@@ -1,4 +1,18 @@
+let disqus = document.createElement('section');
+disqus.id = 'disqus';
+let container = document.createElement('div');
+container.className = 'container';
+disqus.appendChild(container);
+document.getElementsByTagName('body')[0].appendChild(disqus);
+
+let newChild = document.getElementById('disqus');
+let refChild = document.getElementsByTagName('footer')[0];
+document.getElementsByTagName('body')[0].insertBefore(newChild, refChild);
+
 $(function(){
-  $("header").load("/navbar.html");
-  $("footer").load("/footer.html");
+  $('')
+
+  $('#disqus').load('/autoload/disqus.html')
+  $("header").load("/autoload/navbar.html");
+  $("footer").load("/autoload/footer.html");
 });
